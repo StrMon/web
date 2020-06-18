ee331a2905ebc35b9eba7f86a11376b4071b4be6
StrMon <1041011297@qq.com>
2020-06-18 19:56:01 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,6 +1,7 @@
-<html>
-<body>
-Welcome <?php echo $_GET["name"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-</body>
-</html>
+  
+<?php
+  $say = htmlspecialchars($_POST['name']);
+  $to  = htmlspecialchars($_POST['email']);
+
+  echo  $say, ' ', $to;
+?>
